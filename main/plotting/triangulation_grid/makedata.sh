d6fcc32ba57a4e123be83b1f1e92fbd30fe42ef4
Patryk Drozd <drozdpk@gmail.com>
2025-08-16 15:51:46 +0100

    added missig plots

--- main/plotting/triangulation_grid/makedata.sh
+++ main/plotting/triangulation_grid/makedata.sh
@@ -2,7 +2,6 @@
 cd "${0%/*}" || exit 1  # Run from script's directory
 GPUDIR="../../gpu"
 
-make 
 
 SIZESN=("100" "500" "1000")
 SEED=69420
@@ -21,12 +20,15 @@ PLOTDIR="../plotting/triangulation_grid"
 for n in "${SIZESN[@]}"; do
 	for (( d=0; d<$NDISTRIBITIONS; d++)); do
 		cd "$GPUDIR" # need to run gpu code in this directory
+		make 
 
 		echo
 		echo     "$EXEDIR" -n "$n" -s "$SEED" -d "$d" -t "$NTPB"
 		RUN=$( { "$EXEDIR" -n "$n" -s "$SEED" -d "$d" -t "$NTPB" ; } )
 
-		cp ./data/tri.txt $PLOTDATA
+		cp ./data/tri.txt $PLOTDIR
+		#cp ./data/tri.txt $PLOTDATA
+
 		cd $PLOTDIR
 		$HOME/.venv/bin/python3 plot.py save "tri${n}_${d}.png"
 	done
